7e0b151e268be35d5be516c16408dedd8c8e9cde
Shu Yu(HP) <sygoh6@hotmail.com>
2022-04-08 21:24:36 +0800

    Section 13: Dynamic Programing with Udemy (House Robber)

--- src/Section13DynamicProgramming.java
+++ src/Section13DynamicProgramming.java
@@ -13,10 +13,11 @@ public class Section13DynamicProgramming {
         System.out.println("Calculations: "+calculationsOptimized);
 
         //
-        var houseRobber = new HouseRobber(new int[]{7,3,4,5});
+        var houseRobber = new HouseRobber(new int[]{7,3,4,5,4,5,4});
         System.out.println("----House Robber----");
         System.out.println("Solution 1 earn : "+houseRobber.rob_solution1(houseRobber.nums.length-1)+" time complexity: "+houseRobber.timeComplexitySolution1);
         System.out.println("Solution 2 earn : "+houseRobber.rob_solution2(houseRobber.nums.length-1)+" time complexity: "+houseRobber.timeComplexitySolution2);
+        System.out.println("Solution 3 earn : "+houseRobber.rob_solution3()+" time complexity: "+houseRobber.timeComplexitySolution3);
     }
     // time complexity O(2^n)
     private static long fibonacci(int n){
@@ -33,7 +34,7 @@ public class Section13DynamicProgramming {
         if (n<2){
             memo[n] = n;
             return memo[n];
-        };
+        }
 
         calculationsOptimized++;
 
@@ -41,11 +42,13 @@ public class Section13DynamicProgramming {
         return memo[n];
     }
 
+    // #leetcode: 198
     static class HouseRobber{
         private int[] nums;
         private int[] memo1;
         private int timeComplexitySolution1 = 0;
         private int timeComplexitySolution2 = 0;
+        private int timeComplexitySolution3 = 0;
         public HouseRobber(int[] nums){
             this.nums = nums;
             this.memo1 = new int[nums.length];
@@ -70,6 +73,22 @@ public class Section13DynamicProgramming {
             timeComplexitySolution2++;
             memo1[i] = Math.max(rob_solution2(i-1), rob_solution2(i-2)+nums[i]);
             return memo1[i];
+        }
+
+        // time complexity O(n)
+        // space complexity O(1)
+        public int rob_solution3(){
+            if (nums.length == 0) return 0;
+            int prev1 = 0;
+            int prev2 = 0;
+            // prev1, prev2, num [4,2,3,4]
+            for (int num: nums){
+                timeComplexitySolution3++;
+                int tmp = prev1;
+                prev1 = Math.max(prev2+num, prev1); // take the money, skip the house
+                prev2 = tmp;    // prev2 change to prev1 value
+            }
+            return prev1;
         }
     }
 }
